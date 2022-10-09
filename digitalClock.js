1b90d7d05e78828bea72b533b79e7b521546f6d0
Al Amin <mdalamin02536@gmail.com>
2022-10-09 22:28:07 +0600

    Updated digitalclock.js code

--- digitalClock.js
+++ digitalClock.js
@@ -3,15 +3,15 @@ function getCurrentTimer() {
   let Hours = date.getHours();
   const Minutes = date.getMinutes();
   const seconds = date.getSeconds();
+  let midday = "AM";
+  midday = Hours >= 12 ? "PM" : "AM";
 
   Hours = Hours > 12 ? Hours - 12 : Hours;
 
   formattedHours = Hours > 10 ? Hours : "0" + Hours;
   formattedMinutes = Minutes > 10 ? Minutes : "0" + Minutes;
   formattedseconds = seconds > 10 ? seconds : "0" + seconds;
-  //   formattedHours = updateTime(formattedHours);
-  //   formattedMinutes = updateTime(formattedMinutes);
-  //   formattedseconds = updateTime(formattedseconds);
+
   document.getElementById("clock").innerText =
     formattedHours + ":" + formattedMinutes + ":" + formattedseconds;
   const timer = setTimeout(function () {
